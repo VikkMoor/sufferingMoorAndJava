dc06468c7b1295dff781d7e267c44f4c51eea77e
VikkMoor <moorvikk@gmail.com>
2023-11-05 18:48:11 +0500

    primeTempl

---
+++ seminars/sem1/HW/templates/Answer.java
@@ -0,0 +1,28 @@
+package sem1.HW.templates;
+
+// To show prime numbers 1...1000
+
+public class Answer {
+    public void printPrimeNums() {
+        boolean isPrime;
+        for (int i = 2; i < 1000; i++) {
+            isPrime = i == 1;
+            for (int j = 2; j < 1000; j++) {
+                if (i >= j && i % j == 0) {
+                    if (j == i) {
+                        isPrime = true;
+                    }
+                    break;
+                }
+            }
+            if (isPrime) System.out.println(i);
+        }
+    }
+
+    public static void main(String[] args) {
+
+            Answer ans = new Answer();
+            ans.printPrimeNums();
+    }
+}
+
