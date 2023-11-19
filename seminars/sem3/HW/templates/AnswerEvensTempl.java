71d3cfd0fa7f849d5bc15340a0293813c3473eee
VikkMoor <moorvikk@gmail.com>
2023-11-19 16:52:07 +0500

    sem3ansTempl

---
+++ seminars/sem3/HW/templates/AnswerEvensTempl.java
@@ -0,0 +1,37 @@
+package sem3.HW.templates;
+
+import java.util.Arrays;
+import java.util.ArrayList;
+
+// Method for remove even numbers from list:
+
+class AnswerEvensTempl {
+    public static void removeEvenNumbers(Integer[] arr) {
+
+        ArrayList<Integer> ints = new ArrayList<>(Arrays.asList(arr));
+        for (int i = 0; i < ints.size(); i++) {
+            if (ints.get(i) % 2 == 0) {
+                ints.remove(i);
+                i--;
+            }
+        }
+        System.out.println(ints);
+    }
+
+    // It was the other class here for checking and displaying the result on the screen in autotests:
+    public static void main(String[] args) {
+        Integer[] arr = {};
+
+        if (args.length == 0) {
+            // We can change this value and operator when check it in autotest:
+            arr = new Integer[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 20, 31, 100, 29};
+        } else {
+            arr = Arrays.stream(args[0].split(", "))
+                    .map(Integer::parseInt)
+                    .toArray(Integer[]::new);
+        }
+
+        sem3.HW.templates.AnswerEvensTempl ans = new sem3.HW.templates.AnswerEvensTempl();
+        ans.removeEvenNumbers(arr);
+    }
+}
