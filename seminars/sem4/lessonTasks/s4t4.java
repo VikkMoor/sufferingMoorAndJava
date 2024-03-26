b75dc2aa5ab8aacc6589b8f003c7aef30064b436
VikkMoor <moorvikk@gmail.com>
2024-03-27 01:23:30 +0500

    s4t4task

---
+++ seminars/sem4/lessonTasks/s4t4.java
@@ -0,0 +1,13 @@
+package sem4.lessonTasks;
+
+/* Make a stack using an array.
+   Have to write methods:
+   size(), empty(), peek(), pop().
+*/
+
+
+public class s4t4 {
+    public static void main(String[] args) {
+        System.out.println("Meow");
+    }
+}
