dcdf02042b4edd247ae25664bfa0d68a4a8d8752
VikkMoor <moorvikk@gmail.com>
2024-03-26 23:45:32 +0500

    task1count

---
+++ seminars/sem4/lessonTasks/task1count.java
@@ -0,0 +1,26 @@
+package sem4.lessonTasks;
+/* 1. Measure the time it takes for 10000 (and then more) items to be added to ArrayList;
+   2. Measure the time it takes for 10000 (and then more) items to be added to LinkedList
+ */
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class task1count {
+    public static void main(String[] args) {
+        List<Integer> list1 = new ArrayList<>();
+        List<Integer> list2 = new LinkedList<>();
+        System.out.println(work(list1));
+        System.out.println(work(list2));
+    }
+
+    private static long work(List<Integer> list) {
+        long timeStart = System.currentTimeMillis();
+        for (int i = 0; i < 1000000; i++) {
+            list.add(i);
+        }
+        long timeFinish = System.currentTimeMillis();
+        return timeFinish - timeStart;
+    }
+}
