45fe53a455f3c0c14eaea2f395fe8dab135bf86b
VikkMoor <moorvikk@gmail.com>
2024-03-27 00:59:25 +0500

    s4t3stackReady

--- seminars/sem4/lessonTasks/s4t3.java
+++ seminars/sem4/lessonTasks/s4t3.java
@@ -4,8 +4,23 @@ package sem4.lessonTasks;
    2. Same with queue.
 */
 
+import java.util.Arrays;
+import java.util.Stack;
+
 public class s4t3 {
     public static void main(String[] args) {
+        String[] arr = {"1", "2", "3", "4"};
+        printStack(arr);
+
         System.out.println("Meow");
+    }
+
+    static void printStack(String[] arr){
+        Stack<String> stack = new Stack<>();
+        stack.addAll(Arrays.asList(arr));
+//        System.out.println(stack);   - print not LIFO;
+        while (!stack.empty()){
+            System.out.println(stack.pop());  // 'pop' is for getting and deleting last element;
+        }
     }
 }
