4ac85a27f31c9c09f6545badef95b1478ef07f8b
VikkMoor <moorvikk@gmail.com>
2024-03-27 01:15:08 +0500

    s4t3Ready

--- seminars/sem4/lessonTasks/s4t3.java
+++ seminars/sem4/lessonTasks/s4t3.java
@@ -5,22 +5,33 @@ package sem4.lessonTasks;
 */
 
 import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Stack;
 
 public class s4t3 {
     public static void main(String[] args) {
         String[] arr = {"1", "2", "3", "4"};
-        printStack(arr);
+        //printStack(arr);
+        printQueue(arr);
 
-        System.out.println("Meow");
     }
 
-    static void printStack(String[] arr){
+    private static void printQueue(String[] arr) {
+        Queue<String> queue = new LinkedList<>();
+        queue.addAll(Arrays.asList(arr));
+        while (!queue.isEmpty()) {
+            System.out.println(queue.poll()); // FIFO;
+        }
+    }
+
+
+    static void printStack(String[] arr) {
         Stack<String> stack = new Stack<>();
         stack.addAll(Arrays.asList(arr));
 //        System.out.println(stack);   - print not LIFO;
-        while (!stack.empty()){
-            System.out.println(stack.pop());  // 'pop' is for getting and deleting last element;
+        while (!stack.empty()) {
+            System.out.println(stack.pop());  // 'pop' is for getting and deleting last element (LIFO);
         }
     }
 }
