1e2a0ba9bcf4a28990dc9540deadee6d10c20398
VikkMoor <moorvikk@gmail.com>
2024-03-27 00:39:39 +0500

    s4t3writing

---
+++ seminars/sem4/lessonTasks/s4t3.java
@@ -0,0 +1,11 @@
+package sem4.lessonTasks;
+
+/* 1. Write a method to put an array of elements on the stack and output its contents to the console;
+   2. Same with queue.
+*/
+
+public class s4t3 {
+    public static void main(String[] args) {
+        System.out.println("Meow");
+    }
+}
