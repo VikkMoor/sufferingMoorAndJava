556bad7581cc7db67b30fd2c0f1be44b3b753b42
Victoria Moor <moorvikk@gmail.com>
2024-06-15 10:55:54 +0500

    nameComment.java

--- seminars/sem1/lessonTasks/name.java
+++ seminars/sem1/lessonTasks/name.java
@@ -6,8 +6,8 @@ public class name {
     public static void main(String[] args) {
         Scanner inputName = new Scanner(System.in);
         System.out.print("Enter your name: ");
-        String name = inputName.nextLine(); // Each step the scanner will read a new line (if text is
-        // more than one line it will read one by one)
+        String name = inputName.nextLine(); /* Each step the scanner will read a new line (if text is
+                                            more than one line it will read one by one) */
         System.out.printf("Hello, %s!", name);
     }
 }
