1de69450546e163415d1af04abdf0cca23f52e8a
VikkMoor <moorvikk@gmail.com>
2024-08-29 22:03:43 +0500

    sem5 task3

---
+++ seminars/sem5/HW/readyMade/HeapSort.java
@@ -0,0 +1,63 @@
+package sem5.HW.readyMade;
+
+import java.util.Arrays;
+
+public class HeapSort {
+
+    public static void buildTree(int[] tree, int sortLength) {
+        for (int i = sortLength / 2 - 1; i >= 0; i--) {
+            heapify(tree, sortLength, i);
+        }
+    }
+
+    public static void heapify(int[] tree, int sortLength, int rootIndex) {
+        int largest = rootIndex;
+        int leftChild = 2 * rootIndex + 1;
+        int rightChild = 2 * rootIndex + 2;
+
+        if (leftChild < sortLength && tree[leftChild] > tree[largest]) {
+            largest = leftChild;
+        }
+
+        if (rightChild < sortLength && tree[rightChild] > tree[largest]) {
+            largest = rightChild;
+        }
+
+        if (largest != rootIndex) {
+            swap(tree, rootIndex, largest);
+            heapify(tree, sortLength, largest);
+        }
+    }
+
+    public static void heapSort(int[] sortArray, int sortLength) {
+        buildTree(sortArray, sortLength);
+
+        for (int i = sortLength - 1; i >= 0; i--) {
+            swap(sortArray, 0, i);
+            heapify(sortArray, i, 0);
+        }
+    }
+
+    public static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    public static void main(String[] args) {
+        int[] initArray;
+
+        if (args.length == 0) {
+            initArray = new int[]{17, 32, 1, 4, 25, 17, 0, 3, 10, 7, 64, 1};
+        } else {
+            initArray = Arrays.stream(args[0].split(" ")).mapToInt(Integer::parseInt).toArray();
+        }
+
+        System.out.println("Initial array:");
+        System.out.println(Arrays.toString(initArray));
+        HeapSort.heapSort(initArray, initArray.length);
+        System.out.println("Sorted array:");
+        System.out.println(Arrays.toString(initArray));
+    }
+
+}
